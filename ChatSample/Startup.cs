a5a7b5099ae2b266a78e5d23351871bf70280d64
Yunus <yunusozdemir468@gmail.com>
2021-11-23 22:46:08 +0300

    connectionString and some changes

--- ChatSample/Startup.cs
+++ ChatSample/Startup.cs
@@ -1,5 +1,4 @@
-﻿using ChatSample.Hubs;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,7 +11,6 @@ namespace ChatSample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,7 +27,6 @@ namespace ChatSample
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
